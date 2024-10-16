4f29ddbbc7fe4829e4c0ea7beec43f0d65c89f1a
Maxi <maximilianoaffonso06@gmail.com>
2024-10-15 23:46:33 -0300

    entega proyecto final js maxi affonso

--- js/productos.js
+++ js/productos.js
@@ -1,16 +1,28 @@
-// Lista de productos
-const productos = [
-    { id: 1, nombre: "Fotos", precio: 7000, img: "../img/fotos.jpg" },
-    { id: 2, nombre: "Desayuno", precio: 12000, img: "../img/desayuno.jpg" },
-    { id: 3, nombre: "Cuadro", precio: 9000, img: "../img/cuadro4fotos.jpg" },
-    { id: 4, nombre: "Fotos imantadas", precio: 15000, img: "../img/imagenesimantadas.jpg" },
-    { id: 5, nombre: "Álbum de figuritas", precio: 9000, img: "../img/albundefiguritas.jpg" },
-    { id: 6, nombre: "Fotos Spotify", precio: 7500, img: "../img/fotosspotify.jpg" },
-];
+let productos = []; 
 
-// Cargar productos en la página
-function cargarProductos() {
+// Cargar productos desde un archivo JSON utilizando fetch
+function cargarProductosDesdeJSON() {
+    fetch('../json/productos.json')  
+        .then(response => {
+            if (!response.ok) {  
+                throw new Error('Network response was not ok: ' + response.statusText);
+            }
+            return response.json();  
+        })
+        .then(data => {
+            productos = data;  
+            mostrarProductos(); 
+        })
+        .catch(error => {
+            console.error('Error al cargar los productos:', error);
+        });
+}
+
+
+function mostrarProductos() {
     const contenedor = document.getElementById('productos');
+    contenedor.innerHTML = '';  
+
     productos.forEach(producto => {
         const div = document.createElement('div');
         div.classList.add('producto', 'card', 'mb-3');
@@ -37,7 +49,7 @@ function agregarAlCarrito(id) {
     } else {
         carrito.push({ ...producto, cantidad: 1 });
     }
-    
+
     localStorage.setItem('carrito', JSON.stringify(carrito));
     Toastify({
         text: `¡${producto.nombre} ha sido agregado al carrito!`,
@@ -47,13 +59,13 @@ function agregarAlCarrito(id) {
         position: "right", 
         backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
     }).showToast();
-    
 }
 
-// Event listeners
-document.addEventListener('DOMContentLoaded', () => {
-    cargarProductos();
 
+document.addEventListener('DOMContentLoaded', () => {
+    cargarProductosDesdeJSON();  
+
+    // Evento para agregar productos al carrito
     document.getElementById('productos').addEventListener('click', (e) => {
         if (e.target.classList.contains('agregar-carrito')) {
             const id = parseInt(e.target.getAttribute('data-id'));
